4673edef931c94b0820b9e4d2f8c71721cfcfc0f
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-01-16 20:48:44 +0100

    16.01.23.2

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+const path = require("path"); // for constructing path that will be recognised in all operating systems
+
 const express = require("express");
 
+const authRoutes = require("./routes/auth.routes");
+
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); //constructing an absolute path to views folder
+
+app.use(authRoutes);
 
 app.listen(3000);
