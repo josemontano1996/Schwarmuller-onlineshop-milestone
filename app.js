ddc4b4d0ac009e2f7291cbcd4ca4b27a0bdc13f1
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-01-21 20:51:45 +0100

    added csrf token

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const path = require("path"); // for constructing path that will be recognised in all operating systems
 
 const express = require("express");
+const csrf = require("csurf"); // currently not manteined any more, have to check for other one
 
 const db = require("./database/database");
+const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
 
 const authRoutes = require("./routes/auth.routes");
 
@@ -12,6 +14,10 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); //constructing an absolute path to views folder
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false })); //extended:false is for handling just regular form submissions
+
+app.use(csrf());
+app.use(addCsrfTokenMiddleware);
 
 app.use(authRoutes);
 
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,27 @@
+const User = require("../models/user.model");
+
 function getSignUp(req, res) {
   res.render("customer/auth/signup");
 }
 
-function postSignUp(req, res) {
-  
+async function postSignUp(req, res) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city
+  );
+
+  await user.signUp();
+
+  res.redirect("/login");
 }
 
-function getLogin(req, res) {}
+function getLogin(req, res) {
+  res.render("customer/auth/login");
+}
 
 module.exports = {
   getSignUp: getSignUp,
