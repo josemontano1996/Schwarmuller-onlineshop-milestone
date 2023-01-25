5de33799cc9e9a536a1ef04552f5d179ebfe0761
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-01-25 19:08:30 +0100

    25.01.23

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -2,12 +2,12 @@ function checkAuthStatus(req, res, next) {
   const uid = req.session.uid;
 
   if (!uid) {
-    console.log("Fail to add session");
     return next();
   }
 
   res.locals.uid = uid;
   res.locals.isAuth = true;
+  res.locals.isAdmin = req.session.isAdmin;
   next();
 }
 
--- util/authentication.js
+++ util/authentication.js
@@ -1,5 +1,6 @@
 function createUserSession(req, user, action) {
   req.session.uid = user._id.toString(); // available because of the express-session package
+  req.session.isAdmin = user.isAdmin;
   req.session.save(action); //this makes that the action is executed once the session is saved and stored
 }
 
